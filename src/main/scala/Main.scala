215da8fc226dc9a9ef8b6c256a382c2aa0e12ec7
Tim <lztjeans@gmail.com>
2017-11-10 13:36:03 +0800

    update

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,54 @@
-import org.apache.spark.{SparkConf, SparkContext}
+
+//import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+//import org.apache.spark.SparkContext
+import org.apache.spark._
+
 
 object Main {
 
-  val appName: String = "HelloWorld"
-  val master: String = "local"
+  def createContext(appName:String) :SparkSession= {
+    return SparkSession.builder().appName(appName).getOrCreate()
+  }
 
-  //def reduceByKey : T ={}
+  def createContext(appName:String, master:String): SparkContext={
+    return new SparkContext(new SparkConf().setAppName(appName).setMaster(master))
+  }
 
   def main(args: Array[String]): Unit = {
-//    val conf = new SparkConf().setMaster(master)
-//    val sparkSession = SparkSession.builder().appName(appName).config(conf).getOrCreate()
-//    val sc = sparkSession.sparkContext
-    //val sc = new sparkSession().
+    val appName: String = "HelloWorld"
+    val master: String = "local"
+//    val master: String = "spark://localhost:18080"
+
+//    val conf = new SparkConf().setAppName(appName).setMaster(master)
+//    val sc = new SparkContext(conf)
+//    val sc = SparkSession.builder.appName(appName).getOrCreate()
+
+    val sc = createContext(appName,master)
+//    val sc = createContext(appName)
+
+    val inputPath = "file:///tmp/testFiles/*.txt"
+    val outputPath = "file:///tmp/res"
 
 
-    val conf = new SparkConf().setAppName(appName).setMaster(master)
-    val sc = new SparkContext(conf)
+    test1(sc, inputPath, outputPath)
 
-//    val sc = SparkSession.builder.appName("Simple Application").getOrCreate()
-
-    val textFile = sc.textFile("file:///home/Tim/dockerManu.txt")
-    val counts = textFile.flatMap(line => line.split(" "))
-      .map(word => (word, 1))
-      .reduceByKey(_ + _)
-    counts.saveAsTextFile("file:///home/Tim/dock2")
-
-    //textFile.saveAsTextFile("file:///home/Tim/dock2")
-    //sparkSession.close()
-
-
+//    val textFile = sc.read.textFile(inputPath)//dataset
+    //val wordMap = textFile.flatMap(line => line.split(" ")).map(word => (word,1))
+//    println(textFile)
 
 
   }
+
+  private def test1(sc: SparkContext, inputPath: String, outputPath: String) = {
+    val textFile = sc.textFile(inputPath)
+    val counts = textFile
+      .flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+    counts.saveAsTextFile(outputPath)
+
+  }
+
+
 }
